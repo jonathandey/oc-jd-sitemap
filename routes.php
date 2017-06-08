9f65ed49ac15f054a25736b2e403e1470b8f289e
Jonathan Dey <jon@trenttech.co.uk>
2017-06-08 09:56:31 +0100

    Now using sitemap.xml as route

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 use JD\Sitemap\Sitemap;
 
-Route::get('sitemap2.xml', function() {
+Route::get('sitemap.xml', function() {
     return Response::make(Sitemap::generate())
         ->header("Content-Type", "application/xml")
 	;
